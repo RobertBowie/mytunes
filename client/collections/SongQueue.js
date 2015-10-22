72e78fd3b004798b64f1b09ffa22e7c47866fd63
RobertBowie <robertb1903@gmail.com>
2015-10-21 20:32:06 -0700

    AppModel tests pass.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,17 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
-
+  
   initialize: function(){
-  }
+    //when add is called run this enqueue function
+    //model.on('change', this.render, this) or model.on({change: this.render}, this).
+    this.on('add', this.enqueue, this);
+  },
 
+  //enqueue function
+  enqueue: function() {
+    this.push();
+  }
+  //this.pop
+  // Write an enqueue function, which 
+    // take 'this' song and  into the song queue
 });
--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -4,10 +4,11 @@ var LibraryEntryView = Backbone.View.extend({
   tagName: 'tr',
 
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
-
+//this is where the enqueue gets triggered from
   events: {
     'click': function() {
-      this.model.play();
+      this.model.enqueue(); // this.model.play();
+      console.log(this.model.attributes);
     }
   },
 
